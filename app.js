335793b3c97d45e07d3124b90b134cbc3e835f0e
Artem Silka <silkartem@gmail.com>
2023-01-06 00:18:54 -0500

    body parser + middleware

--- app.js
+++ app.js
@@ -11,15 +11,21 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 
+// body parser
+const bodyParser = require("body-parser");
+
 // connect to mongoDB
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(()=> console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req,res) => res.send("Macungie 2023!"));
-
+app.get("/", (req,res) => res.send("Macungie 2023!!"));
+// Express to use routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
+// Middleware for body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Express starts a socket and listen for connections on the path
 app.listen(port,() => console.log(`Server is running on ${port} port`));
